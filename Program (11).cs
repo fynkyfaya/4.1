c372307a7e6e85e0e44c044d7982abf95b6d3736
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-11-10 20:32:14 +0200

    Add files via upload

---
+++ Program (11).cs
@@ -0,0 +1,32 @@
+﻿﻿var firstArray = new double[7];
+var secondArray = new double[9];
+
+for (var i = 0; i < firstArray.Length; i++)
+    firstArray[i] = double.Parse(Console.ReadLine());
+
+for (var i = 0; i < secondArray.Length; i++) 
+    secondArray[i] = double.Parse(Console.ReadLine());
+
+var thirdArray = new double[firstArray.Length + secondArray.Length];
+
+for (var i = 0; i < firstArray.Length; i++) {
+    thirdArray[i] = firstArray[i];
+}
+
+for (var i = firstArray.Length; i < thirdArray.Length; i++) {
+    thirdArray[i] = secondArray[i - firstArray.Length];
+}
+
+double temperance;
+for (var write = 0; write < thirdArray.Length; write++) {
+    for (var sort = 0; sort < thirdArray.Length - 1; sort++) {
+        if (thirdArray[sort] < thirdArray[sort + 1]) {
+            temperance = thirdArray[sort + 1];
+            thirdArray[sort + 1] = thirdArray[sort];
+            thirdArray[sort] = temperance;
+        }
+    }
+}
+
+for (var i = 0; i < thirdArray.Length; i++)
+    Console.Write($"{thirdArray[i]} ");
